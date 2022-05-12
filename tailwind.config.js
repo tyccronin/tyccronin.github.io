9c5eec1e72342fc0dcfc27b96aa0010358b6e5ea
Ty Cronin <tyccronin@gmail.com>
2022-05-12 12:59:46 -0700

    Site Overhaul - Update 2.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      xs: '360px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
